272e11dac2fab610f5db2089318f39512413727e
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-28 10:34:29 -0700

    add html and js file of manipulating

---
+++ manipulating.js
@@ -0,0 +1,4 @@
+// The url you need: 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg'
+const eggLink =document.querySelector('#egg');
+eggLink.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg');
+eggLink.setAttribute('alt', 'chicken');
